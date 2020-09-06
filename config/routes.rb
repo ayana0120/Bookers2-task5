92e0c14b5b034a88e16d9641203f4d0073e2a536
ayana0120 <keichan0501.ayana@gmail.com>
2020-09-06 20:09:22 +0900

    [Fix]ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
 
-  get "/search" => "search#search"
+  get "search" => "searches#search"
+  resources :searches, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
